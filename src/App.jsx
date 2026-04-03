1f3b515b5d3c82d173a1abf6ac3be611d4a0ad38
MerrCraft <161960686+MerrCraft@users.noreply.github.com>
2026-04-03 14:36:00 +0000

    Reworked for react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom"
 
 import { Home } from "./pages/home.jsx"
 import { Chat } from "./pages/chat.jsx"
 
 function App() {
   return (
-    <BrowserRouter style={{ height: '100vh', "background-color": "#111111"}}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <div className="app-shell" style={{ minHeight: '100vh', backgroundColor: '#111111' }}>
+        <header className="app-nav">
+          <Link to="/" className="app-link">Home</Link>
+          <Link to="/chat" className="app-link">Chat</Link>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   )
 }
 
